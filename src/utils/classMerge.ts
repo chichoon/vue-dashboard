45cdf03fd11f997c9c9200aacf27f63e692796b1
chichoon <chichoon.choi@gmail.com>
2023-11-20 11:41:54 +0900

    fix: :art: classMerge 클래스 undefined 도 들어올 수 있게끔 수정

--- src/utils/classMerge.ts
+++ src/utils/classMerge.ts
@@ -1,6 +1,8 @@
 import type { ClassType } from '@/types/common';
 
-export function classMerge(...args: ClassType[]): (string | Record<string, boolean>)[] {
+export function classMerge(
+  ...args: (ClassType | undefined)[]
+): (string | Record<string, boolean>)[] {
   return args.reduce<(string | Record<string, boolean>)[]>((acc, cur) => {
     if (Array.isArray(cur)) {
       acc.push(...cur);
