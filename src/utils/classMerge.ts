df2350983369d266b5985da49acbac0aac1bc7b8
chichoon <chichoon.choi@gmail.com>
2023-11-20 10:46:34 +0900

    feat: :sparkles: 클래스 합치는 함수 추가

---
+++ src/utils/classMerge.ts
@@ -0,0 +1,14 @@
+import type { ClassType } from '@/types/common';
+
+export function classMerge(...args: ClassType[]): (string | Record<string, boolean>)[] {
+  return args.reduce<(string | Record<string, boolean>)[]>((acc, cur) => {
+    if (Array.isArray(cur)) {
+      acc.push(...cur);
+    } else if (typeof cur === 'object') {
+      acc.push(cur);
+    } else if (typeof cur === 'string') {
+      acc.push(cur);
+    }
+    return acc;
+  }, []);
+}
