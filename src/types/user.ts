071cdfbbdf2b50b3d19a32b567c98de534255c50
chichoon <chichoon.choi@gmail.com>
2023-11-21 19:31:12 +0900

    feat: :label: 유저 타입 추가

---
+++ src/types/user.ts
@@ -0,0 +1,12 @@
+export interface UserType {
+  username: string;
+  firstName: string;
+  lastName: string;
+  bio: string;
+  address?: string;
+  city?: string;
+  country?: string;
+  postalCode?: string;
+  email?: string;
+  company?: string;
+}
