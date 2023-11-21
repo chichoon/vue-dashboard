0be42ae0235cfe440d3fbeca3296674a948e6c08
chichoon <chichoon.choi@gmail.com>
2023-11-21 19:30:54 +0900

    feat: :sparkles: 사용자 정보 뷰에 대시보드 컴포넌트 추가

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,7 +1,35 @@
 <script setup lang="ts">
-import ButtonComponent from '@/components/atoms/ButtonComponent.vue';
+import { DashboardTitleItem } from '@/components/molecules/Dashboard';
+import UserForm from '@/components/molecules/UserFormVuex.vue.js';
 </script>
 
 <template>
-  <ButtonComponent color="warning" text="hello" />
+  <main class="user-view">
+    <DashboardTitleItem
+      class="user-view__form"
+      color="success"
+      title="Edit Profile With Vuex"
+      subtitle="Complete your profile"
+    >
+      <template #bottom>
+        <UserForm />
+      </template>
+    </DashboardTitleItem>
+  </main>
 </template>
+
+<style scoped lang="scss">
+.user-view {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 30px 15px;
+
+  @media screen and (min-width: 1281px) {
+    &__form {
+      min-width: 66.6666%;
+      max-width: 66.6666%;
+      width: 66.6666%;
+    }
+  }
+}
+</style>
