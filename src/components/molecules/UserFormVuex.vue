d1dfc8b175c68bad9a8d9d50f948154e58253fe0
chichoon <chichoon.choi@gmail.com>
2023-11-21 19:28:44 +0900

    feat: :sparkles: Pinia, vuex 이용한 사용자 입력폼 추가 (파일만)

---
+++ src/components/molecules/UserFormVuex.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import ButtonComponent from '../atoms/ButtonComponent.vue';
+</script>
+
+<template>
+  <div>
+    <form>
+      <ev-text-field name="aaa" />
+      <ButtonComponent color="success" text="submit" />
+    </form>
+  </div>
+</template>
