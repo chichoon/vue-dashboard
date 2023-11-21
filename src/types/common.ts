9419bce690081204cb7a98ade77a4c933771df6c
chichoon <chichoon.choi@gmail.com>
2023-11-21 12:11:18 +0900

    feat: :label: 대시보드 색상 타입 추가

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,1 +1,2 @@
 export type ClassType = string | Record<string, boolean> | (string | Record<string, boolean>)[];
+export type DashboardColorType = 'info' | 'success' | 'warning' | 'danger' | 'primary';
