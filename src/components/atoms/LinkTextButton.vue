75f391a996ce085fb4dd5e9fb3d14801094d7e26
chichoon <chichoon.choi@gmail.com>
2023-11-21 09:39:01 +0900

    feat: :sparkles: 푸터 바 춛가

---
+++ src/components/atoms/LinkTextButton.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+interface Props {
+  to?: string;
+  text: string;
+}
+
+const props = defineProps<Props>();
+</script>
+<template>
+  <RouterLink :to="props.to ?? ''" class="link-text">
+    <span>{{ props.text }}</span>
+  </RouterLink>
+</template>
+
+<style scoped lang="scss">
+.link-text {
+  display: block;
+  padding: 15px;
+  text-decoration: none;
+
+  & > span {
+    font-size: var(--font-sm);
+    color: var(--text-primary);
+    font-weight: 500;
+    text-transform: uppercase;
+
+    &:hover {
+      color: var(--notification-primary);
+    }
+  }
+}
+</style>
---
+++ src/components/molecules/FooterBar.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import LinkTextButton from '../atoms/LinkTextButton.vue';
+import HeartIcon from '../icons/HeartIcon.vue';
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="footer__left">
+      <LinkTextButton text="creative tim" />
+      <LinkTextButton text="about us" />
+      <LinkTextButton text="blog" />
+      <LinkTextButton text="licenses" />
+    </div>
+    <div class="footer__right">
+      <span>© 2023 Creative Tim, made with</span>
+      <HeartIcon />
+      <span>for a better web</span>
+    </div>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+.footer {
+  display: flex;
+  flex-direction: column;
+  padding: 15px 30px;
+
+  @media screen and (min-width: 1025px) {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  & > div {
+    flex: 1;
+  }
+
+  &__left {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+  }
+
+  &__right {
+    text-align: end;
+    color: var(--text-secondary);
+    padding: 15px 0;
+
+    & > svg {
+      width: 15px;
+      height: 15px;
+      margin: 0 5px;
+    }
+  }
+}
+</style>
