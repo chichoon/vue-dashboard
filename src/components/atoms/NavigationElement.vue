f44fa7bb17da3bb81d90ad762d6d99f8bc45f19a
chichoon <chichoon.choi@gmail.com>
2023-11-22 17:15:50 +0900

    feat: :sparkles: 설정한 색상에 따라 GNB 버튼 색상 변경

--- src/components/atoms/NavigationElement.vue
+++ src/components/atoms/NavigationElement.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import { storeToRefs } from 'pinia';
 import { RouterLink, useRoute } from 'vue-router';
+
+import { useSettings } from '@/stores/useSettings';
 
 interface Props {
   text: string;
@@ -9,11 +12,23 @@ interface Props {
 defineProps<Props>();
 
 const route = useRoute();
+const settingsStore = useSettings();
+const { settings } = storeToRefs(settingsStore);
 </script>
 
 <template>
   <li class="list">
-    <RouterLink :to="to" :class="['list__link', { 'list__link--focused': route.path === to }]">
+    <RouterLink
+      :to="to"
+      :class="[
+        'list__link',
+        { 'list__link--success': route.path === to && settings.color === 'success' },
+        { 'list__link--info': route.path === to && settings.color === 'info' },
+        { 'list__link--primary': route.path === to && settings.color === 'primary' },
+        { 'list__link--warning': route.path === to && settings.color === 'warning' },
+        { 'list__link--danger': route.path === to && settings.color === 'danger' }
+      ]"
+    >
       <slot />
       <span>{{ text }}</span>
     </RouterLink>
@@ -45,8 +60,28 @@ const route = useRoute();
       background-color: var(--bg-hover);
     }
 
-    &--focused {
+    &--success {
       background-color: var(--notification-success) !important;
+      color: var(--white);
+    }
+
+    &--info {
+      background-color: var(--notification-info) !important;
+      color: var(--white);
+    }
+
+    &--primary {
+      background-color: var(--notification-primary) !important;
+      color: var(--white);
+    }
+
+    &--warning {
+      background-color: var(--notification-warning) !important;
+      color: var(--white);
+    }
+
+    &--danger {
+      background-color: var(--notification-danger) !important;
       color: var(--white);
     }
   }
