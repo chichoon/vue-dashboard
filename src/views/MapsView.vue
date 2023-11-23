4dd437895895a903dd626fc20ffcf4fe9800b3fa
chichoon <chichoon.choi@gmail.com>
2023-11-23 10:47:38 +0900

    feat: :sparkles: 지도 화면 그냥 간단하게 끝

--- src/views/MapsView.vue
+++ src/views/MapsView.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="maps-view"></div>
+  <div class="maps-view">
+    <img src="/map.png" alt="balsan map" />
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.maps-view {
+  display: flex;
+  flex-direction: column;
+  padding: 30px 15px;
+}
+</style>
